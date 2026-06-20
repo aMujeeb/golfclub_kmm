0b4f8aea5c60a7fb78108cc6c59517e12046d4c7
Mujee <mujee2@gmail.com>
2026-06-20 21:02:39 +0800

    InitKoin() to iOS and Run application on iOS

--- shared/src/commonMain/kotlin/com/mujapps/golfgarage/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/mujapps/golfgarage/di/AppModule.kt
@@ -3,9 +3,17 @@ package com.mujapps.golfgarage.di
 import com.mujapps.data.di.dataModule
 import com.mujapps.domain.di.domainModule
 import com.mujapps.presentation.di.presentationModule
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
 
 val appModule = listOf(
     dataModule,
     domainModule,
     presentationModule
-)   
+)
+
+fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
+    startKoin {
+        appDeclaration()
+        modules(appModule)
+    }   
--- shared/src/iosMain/kotlin/com/mujapps/golfgarage/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/mujapps/golfgarage/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.mujapps.golfgarage
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.mujapps.golfgarage.di.initKoin
 
 fun MainViewController() = ComposeUIViewController { App() }
+
+class IOSKoinInitializer {
+    fun initialize() {
+        initKoin()
+    }
+}
